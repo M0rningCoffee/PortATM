a2ec9775d735247224504f23de9f58ad5319f6d3
Cafezin <pedroviniciusdejesuslima1227@gmail.com>
2026-06-05 22:48:36 -0300

    ajustes script sql

--- Create_atm.sql
+++ Create_atm.sql
@@ -6,9 +6,11 @@ create table contas
 (
 	id_conta int auto_increment,
 	cpf varchar(14) not null,
-	saldo float default 0,
-	cartao int not null,
-	pin int not null,
+	saldo decimal default 0,
+	cartao varchar(16) not null,
+	data_nasc varchar(10) not null,
+	nome_completo varchar(100) not null,
+	pin int(6) not null,
 	agencia varchar(6) not null,
 	cc varchar(7) not null,
 	pix bool default true,
@@ -23,7 +25,7 @@ create table registros
 	id_conta_to int not null, 
 	id_conta_from int not null,
 	operacao varchar(64) check(operacao = "pix" or operacao = "transferência" or operacao = "saque" or operacao = "depósito"),
-	valor float not null,
+	valor decimal default 0,
 	primary key (id_registros),
 	foreign key (id_conta_to) references contas(id_conta),
 	foreign key (id_conta_from) references contas(id_conta)
@@ -31,8 +33,8 @@ create table registros
 
 
 
-insert into contas(cpf, cartao, pin, agencia, cc) 
-values ("111.111.111-11", 123456, 123, 1111-1, 11111-1);
+insert into contas(cpf, cartao, data_nasc, nome_completo, pin, agencia, cc) 
+values ("111.111.111-11", "1111222233334444", "2006-12-12", "Pedro Vinicius", 123456, "1111-1", "11111-1");
 
 insert into registros(id_conta_to, id_conta_from, operacao, valor)
 values (1, 1, "depósito", 0);
@@ -41,6 +43,7 @@ insert into registros(id_conta_to, id_conta_from, operacao, valor)
 values (1, 1, "dinheiro", 10);
 
 select * from contas;
+update contas set saldo = 10000 where id_conta = 1; 
 select saldo from contas where id_conta = 1;
 select * from registros;
 
